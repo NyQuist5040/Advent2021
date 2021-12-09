eb2152d303c9d0ced9613a88e25c93b12661ea0f
qjutard <quentin.jutard@yahoo.fr>
2021-12-09 11:03:24 +0100

    day 9 question 1

---
+++ 09/main.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+f = open("input.txt", "r")
+L = f.read().split("\n")[:-1]
+f.close()
+
+map = np.ones((len(L)+2, len(L[0])+2), np.int32) * 10
+
+for i in range(len(L)) :
+    for j in range(len(L[i])) :
+        map[i+1,j+1] = int(L[i][j])
+
+isLowpoint = np.zeros(map.shape, np.bool)
+
+for i in range(1, map.shape[0]-1) :
+    for j in range(1, map.shape[1]-1) :
+        isLowpoint[i,j] = map[i,j] < map[i+1,j] and map[i,j] < map[i-1,j] and \
+                          map[i,j] < map[i,j+1] and map[i,j] < map[i,j-1]
+
+### Question 1
+print(np.sum(map[isLowpoint] + 1))
