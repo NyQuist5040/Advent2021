a5b432742dd0cd5c437ae7fa9eda3e9c83b8b27e
qjutard <quentin.jutard@yahoo.fr>
2021-12-10 14:33:16 +0100

    day 10 question 1

---
+++ 10/main.py
@@ -0,0 +1,46 @@
+f = open("input.txt", "r")
+L = f.read().split("\n")[:-1]
+f.close()
+
+matches = { "(": ")",
+            "[": "]",
+            "{": "}",
+            "<": ">"
+}
+
+def testLine(line) :
+    bracketPile = []
+    for char in line :
+        if char in matches.keys() : #opening character
+            bracketPile.append(char)
+        elif len(bracketPile) == 0 : # no bracket to close
+            return False, char
+        elif matches[bracketPile[-1]] == char : # the character closes the opened bracket
+                bracketPile.pop()
+        else :
+            return False, char
+    # no bracket error detected
+    return True, ""
+
+numFails = { ")": 0,
+             "]": 0,
+             "}": 0,
+             ">": 0
+}
+
+scoreFails = { ")": 3,
+               "]": 57,
+               "}": 1197,
+               ">": 25137
+}
+
+score = 0
+
+for line in L :
+    isCorrect, failChar = testLine(line)
+    if not isCorrect :
+        numFails[failChar] += 1
+        score += scoreFails[failChar]
+
+### Question 1
+print(score)
