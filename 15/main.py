44779cf622f21ce83b0a1298d0a5e00740ecff75
qjutard <quentin.jutard@yahoo.fr>
2021-12-15 22:52:50 +0100

    new exploration method seems to work but is slow

--- 15/main.py
+++ 15/main.py
@@ -52,16 +52,81 @@ def findExit(position: (int, int), worthExploring: np.array, riskMap: np.array) 
 #print(path, danger-riskMap[0,0])
 
 
-minDangerMap = np.zeros(riskMap.shape, np.int32)
-m,n = riskMap.shape
-for i in range(m) :
-    minDangerMap[i, n-1] = np.sum(riskMap[i:, n-1])
-for j in range(n) :
-    minDangerMap[m-1, j] = np.sum(riskMap[m-1, j:])
+def minMap(riskMap) :
+    minDangerMap = np.zeros(riskMap.shape, np.int32)
+    m,n = riskMap.shape
+    for i in range(m) :
+        minDangerMap[i, n-1] = np.sum(riskMap[i:, n-1])
+    for j in range(n) :
+        minDangerMap[m-1, j] = np.sum(riskMap[m-1, j:])
 
-for i in range(m-2, -1, -1) :
-    for j in range(n-2, -1, -1) :
-        minDangerMap[i,j] = riskMap[i,j] + min(minDangerMap[i+1, j], minDangerMap[i, j+1])
+    for i in range(m-2, -1, -1) :
+        for j in range(n-2, -1, -1) :
+            minDangerMap[i,j] = riskMap[i,j] + min(minDangerMap[i+1, j], minDangerMap[i, j+1])
+
+    return minDangerMap
 
 ### Question 1
+minDangerMap = minMap(riskMap)
 print(minDangerMap[0,0] - riskMap[0,0])
+
+largeRiskMap = [[None for _ in range(5)] for _ in range(5) ]
+for i in range(5) :
+    for j in range(5) :
+        largeRiskMap[i][j] = (riskMap + i + j - 1) % 9 + 1
+largeRiskMap = np.block(largeRiskMap)
+print(largeRiskMap)
+
+def minMapPropagation(riskMap) :
+    minDangerMap = np.zeros(riskMap.shape, np.int32)
+    m,n = riskMap.shape
+    minDangerMap[m-1, n-1] = riskMap[m-1, n-1]
+
+    while True :
+        # Find the point on the border with minimal danger
+        iMin, jMin = 0, 0
+        valMin = 99999
+        for i in range(m) :
+            for j in range(n) :
+
+                # Check that it has been explored
+                if minDangerMap[i, j] == 0 :
+                    continue
+
+                onTheBorder = False
+                for iPlus, jPlus in zip([0,0,1,-1], [1,-1,0,0]) :
+                    iNew = i + iPlus
+                    jNew = j + jPlus
+                    if iNew >= 0 and iNew < m and jNew >= 0 and jNew < n \
+                            and minDangerMap[iNew, jNew] == 0 :
+                        onTheBorder = True
+                        break # no need
+
+                if not onTheBorder :
+                    continue
+
+                if minDangerMap[i, j] < valMin :
+                    iMin, jMin = i, j
+                    valMin = minDangerMap[i, j]
+
+        # All non explored neighbours can be filled
+        for iPlus, jPlus in zip([0,0,1,-1], [1,-1,0,0]) :
+            iNew = iMin + iPlus
+            jNew = jMin + jPlus
+            if iNew >= 0 and iNew < m and jNew >= 0 and jNew < n \
+                    and minDangerMap[iNew, jNew] == 0 :
+                minDangerMap[iNew, jNew] = minDangerMap[iMin, jMin] + riskMap[iNew, jNew]
+
+        # Check if all the map is filled
+        print(np.sum(minDangerMap == 0))
+        if np.sum(minDangerMap == 0) == 0 :
+            break
+
+    return minDangerMap
+
+### Question 2
+#largeMinDangerMap = minMap(largeRiskMap)
+largeMinDangerMap = minMapPropagation(largeRiskMap)
+#largeMinDangerMap = minMapPropagation(riskMap)
+print(largeMinDangerMap[0,0] - largeRiskMap[0,0])
+print(largeMinDangerMap)
